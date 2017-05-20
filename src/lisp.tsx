24eb85793235ade3dd9401d09c31ee0a70446fff
Rick Carlino <rick.carlino@gmail.com>
2017-05-19 21:43:52 -0500

    expr() kind of works

--- src/lisp.tsx
+++ src/lisp.tsx
@@ -1,11 +1,11 @@
 /** Tag an atom with an identifier. */
 export enum AtomType { Nil, Pair, Symbol, Integer }
 export interface Cons { car: Atom; cdr: Atom; }
-export interface IntegerAtom { kind: AtomType.Integer; value: number; }
+export interface NumberAtom { kind: AtomType.Integer; value: number; }
 export interface NilAtom { kind: AtomType.Nil; value: AtomType.Nil; }
 export interface Pair { kind: AtomType.Pair; value: Cons; }
 export interface SymbolAtom { kind: AtomType.Symbol; value: string; }
-export type Atom = NilAtom | SymbolAtom | IntegerAtom | Pair;
+export type Atom = NilAtom | SymbolAtom | NumberAtom | Pair;
 
 export let car = (p: Pair): Atom => p.value.car;
 export let cdr = (p: Pair): Atom => p.value.cdr;
@@ -13,30 +13,45 @@ export let cdr = (p: Pair): Atom => p.value.cdr;
 /** ...is Nil predicate. */
 export let nilp = (p: Atom): p is NilAtom => p.kind == AtomType.Nil;
 export let isPair = (p: Atom): p is Pair => p.kind == AtomType.Pair;
+
 /** "cons"truct a pair. */
 export function cons(car: Atom, cdr: Atom): Pair {
   return { kind: AtomType.Pair, value: { car, cdr } };
 }
 
-export const nil: NilAtom = { kind: AtomType.Nil, value: AtomType.Nil };
+export function nil(): NilAtom {
+  return { kind: AtomType.Nil, value: AtomType.Nil };
+};
 
-export function int(value: number): IntegerAtom {
+export function num(value: number): NumberAtom {
   return { kind: AtomType.Integer, value };
 }
 
-export function symbol(value: string): SymbolAtom {
+export function sym(value: string): SymbolAtom {
   return { kind: AtomType.Symbol, value };
 }
 
-let SYMBOL_TABLE: { [name: string]: SymbolAtom | undefined } = {};
+export function expr(value: string, ...body: Atom[]): Expression {
+  return {
+    kind: FormKind.expr,
+    predicate: { kind: AtomType.Symbol, value },
+    body
+  };
+}
 
-export function make_symbol(value: string): SymbolAtom {
-  let x = SYMBOL_TABLE[value];
+type SYMBOL_TABLE = { [name: string]: SymbolAtom | undefined };
+interface Environment {
+  parent: undefined | Environment;
+  bindings: SYMBOL_TABLE;
+}
+
+export function make_symbol(value: string, env: Environment): SymbolAtom {
+  let x = env.bindings[value];
   if (x) {
     return x;
   } else {
     x = { kind: AtomType.Symbol, value };
-    SYMBOL_TABLE[value] = x;
+    env.bindings[value] = x;
     return x;
   };
 }
@@ -53,23 +68,23 @@ enum Err {
 };
 
 export function env_create(parent: Pair | NilAtom): Pair {
-  return cons(parent, nil);
+  return cons(parent, nil());
 }
 
-function env_get(env: Pair, symbol: SymbolAtom): Err {
+function envGet(env: Environment, symbol: SymbolAtom): Err {
   throw new Error("Throw an exception for unbound stuff.?");
 }
 
-export function env_set(env: Pair, symbol: SymbolAtom, value: Atom): Err {
+export function envSet(env: Pair, symbol: SymbolAtom, value: Atom): Err {
   throw new Error("BRB");
 }
 
 /** Check if a list is a "proper list" */
-export function listp(expr: Atom): number {
+export function listP(expr: Atom): number {
   throw new Error("BRB");
 }
 
-function eval_expr(expr: Atom, env: Atom, result: Atom): Err {
+function evalExpr(expr: Expression, env: SYMBOL_TABLE): Atom | Err {
   throw new Error("BRB");
 }
 
@@ -85,13 +100,17 @@ enum FormKind {
 type Form = Expression | Quote;
 
 interface Expression {
-  kind: FormKind.expr,
+  kind: FormKind.expr;
   predicate: SymbolAtom;
   body: Atom[];
 }
 
 interface Quote {
-  kind: FormKind.quote,
+  kind: FormKind.quote;
   predicate: SymbolAtom;
   body: Atom[];
 }
+
+let myApp = expr("+", num(1), num(2));
+
+evalExpr(myApp, {});
