d433e8289f88d8e0fe36d287ddbb83c6760996a0
Rick Carlino <rick.carlino@gmail.com>
2017-06-23 20:04:31 -0500

    Opps js sourcemaps

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var react_dom_1 = require("react-dom");
-var main_component_1 = require("./main_component");
-react_dom_1.render(React.createElement(main_component_1.Wow, null), document.getElementById("wow"));
-//# sourceMappingURL=index.js.map
